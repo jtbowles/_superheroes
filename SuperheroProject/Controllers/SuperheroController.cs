c3d1ed05e3e3272c89b88b0e80868e28817fcdd7
Jason Bowles <jbowles96@gmail.com>
2019-09-27 14:26:45 -0500

    all four CRUD Views made for Superhero View folder

---
+++ SuperheroProject/Controllers/SuperheroController.cs
@@ -0,0 +1,90 @@
+﻿using SuperheroProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperheroProject.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        // GET: Superhero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superhero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superhero/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Superhero/Create
+        [HttpPost]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
